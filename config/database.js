bc51939f845bb9e4fe16dee1e074127bd3d16a31
Biibiiana <bibiana.antequera@gmail.com>
2025-06-12 14:58:33 +0200

    Other try

--- config/database.js
+++ config/database.js
@@ -6,6 +6,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
+      family: 4,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
     });
